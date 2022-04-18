b2695fc29959597280d3e1363ce5dc68c836a01e
Alina <pisarenko.alina@gmail.com>
2022-04-18 13:45:19 -0400

    added js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const form = document.querySelector("#search-form");
+let image = document.querySelector("#photo-holder");
+let drinkInfo = document.querySelector("#info");
+let table = document.querySelector("#cocktail-info");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e.target["new-name"].value);
   fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${e.target["new-name"].value}`
   )
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => displayDrink(data))
     .catch((err) => console.log(err));
 });
+
+function displayDrink(data) {
+  for (i = 0; i < data.drinks.length; i++) {
+    let img = document.createElement("img");
+    let drinkName = document.createElement("h2");
+    img.src = data.drinks[i].strDrinkThumb;
+    drinkName.textContent = data.drinks[i].strDrink;
+    img.className = "display-image";
+    image.append(img);
+    drinkInfo.append(drinkName);
+    table.append(img, drinkName);
+  }
+}
